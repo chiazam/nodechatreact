8e11d572e72a15b1ca9b59e62f6612297ac3e270
chiazam <54377599+chiazam@users.noreply.github.com>
2022-11-07 15:07:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-       <section class="text-3xl font-bold underline">fuck you</section>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//        <section class="text-3xl font-bold underline">fuck you</section>
+
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+let App = ()=>{
+
+
+
+};
 
 export default App;
