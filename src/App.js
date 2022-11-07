d61578db9b597e5acd2cf72340bbc81bc8eb0768
chiazam <54377599+chiazam@users.noreply.github.com>
2022-11-07 11:57:17 +0100

    fghj

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
+       <section class="text-3xl font-bold underline">fuck you</section>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
